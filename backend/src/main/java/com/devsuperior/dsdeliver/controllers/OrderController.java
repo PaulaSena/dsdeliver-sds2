2e971742195e9c4a95d35b308595fd5116ff7d42
Paula Sena <senasilvaanapaula@gmail.com>
2021-02-25 11:07:13 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
 import com.devsuperior.dsdeliver.services.OrderService;
@@ -22,6 +26,14 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAll(){
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list); 
-				
 	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert (@RequestBody OrderDTO dto){ /* Body Json */
+		dto = service.insert(dto);	/* URI Código 201 de inserção ao banco de dados | recurso criado*/
+		URI uri  = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);/* Criar new request no postman e validar no http://localhost:8080/h2-console | Body row Json*/
+	}
+	
 }
--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,4 +1,5 @@
 package com.devsuperior.dsdeliver.services;
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 
 @Service
@@ -18,7 +23,10 @@ public class OrderService {
 	/*injeção instanciar corretamente a dependencia*/
 	
 	@Autowired
-	private OrderRepository repository;
+	private OrderRepository  repository;
+	
+	@Autowired
+	private ProductRepository  productRepository;
 	
 	public OrderService(OrderRepository repository) {
 		this.repository = repository;
@@ -30,5 +38,16 @@ public class OrderService {
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
 	
+	@Transactional /* Método para inserir novo pedido já associado com os produtos dele*/
+	public OrderDTO insert(OrderDTO dto)  {
+		Order order = new Order (null, dto.getAddress(), dto.getLatitude(),dto.getLongitude(),
+		        Instant.now(), OrderStatus.PENDING); 
+		for(ProductDTO p : dto.getProducts() ) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
 }
 
